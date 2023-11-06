ab262853e78d45a5f53427c9d04ae44df1eef1a1
Andy-Aseye <andyaseye@gmail.com>
2023-11-06 22:18:34 +0000

    firebase-setup

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Andy\\Desktop\\portfolio\\.cache\\dev-404-page.js"))),
   "component---src-pages-index-jsx": hot(preferDefault(require("C:\\Users\\Andy\\Desktop\\portfolio\\src\\pages\\index.jsx")))
 }
 
